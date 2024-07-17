56976e1970f8060cacb8cd6d16e9701c1f3ae0ea
Joker2Yue <Joker2Yue@qq.com>
2024-07-17 19:58:50 +0800

    初始化

---
+++ src/main/java/com/xuntu/blog/XunTuApplication.java
@@ -0,0 +1,13 @@
+package com.xuntu.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XunTuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XunTuApplication.class, args);
+    }
+
+}
